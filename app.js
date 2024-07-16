02fdfb7dc556fb71c3b20520e8b37c914dad9436
Mikael Vaikmae <mikael.vaikmae@gmail.com>
2024-07-16 10:38:02 +0300

    require ejs

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+
 app.set('layout', 'boilerplate');
 app.use(expressLayouts);
 
